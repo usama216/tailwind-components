92bd43ec7893a4db2306e3347d37a8f5ad159dc6
Usama Jawad <usamajatt16@gmail.com>
2024-09-04 04:45:50 +0500

    code uploaded

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-3xl font-bold underline">
+      Hello world!
+    </p>
+        </>
+  )
+}
+
+export default App
